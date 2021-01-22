c1e347fa48dfec41e43602f70f035b53df2f7061
Rodrigo Mardones <ext_rigmardones@falabella.cl>
2021-01-22 17:08:44 -0300

    cambios

--- arbol.c
+++ arbol.c
@@ -2,13 +2,18 @@
 #include "stdio.h"
 #include "string.h"
 
-
 typedef struct Arbol{
     char *esp;
     char *ing;
     int peso;
-    struct Arbol *izquierdo;
-    struct Arbol *derecho;
+
+    struct Arbol *padreEsp;
+    struct Arbol *izquierdoEsp;
+    struct Arbol *derechoEsp;
+
+    struct Arbol *padreIng;
+    struct Arbol *izquierdoIng;
+    struct Arbol *derechoIng;
 } arbol;
 
 arbol *crearArbol(char *esp, char *ing){
@@ -19,65 +24,27 @@ arbol *crearArbol(char *esp, char *ing){
     a->ing = malloc(sizeof(ing));
     strcpy(a->ing, ing);
 
-    a->izquierdo = NULL;
-    a->derecho = NULL;
-    a->peso = 1;
+    a->padreEsp = NULL;
+    a->izquierdoEsp = NULL;
+    a->derechoEsp = NULL;
+
+    a->padreIng = NULL;
+    a->izquierdoIng = NULL;
+    a->derechoIng = NULL;
+
     return a;
 }
 
-int height(arbol *a) {
-  if (a == NULL)
-    return 0;
-  return a->peso;
-}
 
-int max(int a, int b) {
-  return (a > b) ? a : b;
-}
-
-arbol *derechoRotate(arbol *y) {
-  arbol *x = y->izquierdo;
-  arbol *T2 = x->derecho;
-
-  x->derecho = y;
-  y->izquierdo = T2;
-
-  y->peso = max(height(y->izquierdo), height(y->derecho)) + 1;
-  x->peso = max(height(x->izquierdo), height(x->derecho)) + 1;
-
-  return x;
-}
-
-// izquierdo rotate
-arbol *izquierdoRotate(arbol *x) {
-  arbol *y = x->derecho;
-  arbol *T2 = y->izquierdo;
-
-  y->izquierdo = x;
-  x->derecho = T2;
-
-  x->peso = max(height(x->izquierdo), height(x->derecho)) + 1;
-  y->peso = max(height(y->izquierdo), height(y->derecho)) + 1;
-
-  return y;
-}
-
-int getBalance(arbol *a) {
-  if (a == NULL)
-    return 0;
-  return height(a->izquierdo) - height(a->derecho);
-}
-
-
-arbol *agregarHoja(arbol *a, char *esp, char *ing){
+arbol *agregarHoja(arbol *a, char *esp,char *ing){
     if(a == NULL){
         return crearArbol(esp, ing);
     }
     if(strcmp(a->esp, esp) < 0){
-        a->derecho = agregarHoja(a->derecho, esp, ing);
+        a->izquierdoEsp = agregarHoja(a->izquierdoEsp, esp, ing);
     }
-    else{
-        a->izquierdo = agregarHoja(a->izquierdo, esp, ing);
+    else if(strcmp(a->esp, esp) > 0){
+        a->derechoEsp = agregarHoja(a->derechoEsp, esp, ing);
     }
     return a;
 }
@@ -87,17 +54,16 @@ arbol *agregarHojaIngles(arbol *a, char *esp, char *ing){
         return crearArbol(esp, ing);
     }
     if(strcmp(a->ing, ing) < 0){
-        a->derecho = agregarHojaIngles(a->derecho, esp, ing);
+        a->izquierdoIng = agregarHojaIngles(a->izquierdoIng,esp, ing);
+
     }
-    else{
-        a->izquierdo = agregarHojaIngles(a->izquierdo, esp, ing);
+    else if(strcmp(a->ing, ing) >= 0){
+        a->derechoEsp = agregarHoja(a->derechoEsp, esp, ing);
     }
     return a;
 }
 
-
-
-arbol *rellenarArbol(int flag){
+arbol *rellenarArbol(){
     FILE *fptr;
     char *esp = malloc(1024 * sizeof(char));
     char *ing = malloc(1024 * sizeof(char));
@@ -105,14 +71,70 @@ arbol *rellenarArbol(int flag){
     fptr = fopen("entrada.in", "r");
     while (fscanf(fptr,"%s %s",esp, ing) != EOF){
         // printf("%s  %s \n", esp, ing);
-        if(flag){
-            a = agregarHoja(a, esp, ing);
-        }else{
-            a = agregarHojaIngles(a, esp, ing);
-        }
+        a = agregarHoja(a, esp, ing);
+        a = agregarHojaIngles(a, esp, ing);
+        
     }
+    inorder(a,a);
+    inorder(inorderIngles);
     fclose(fptr);
     free(esp);
     free(ing);
     return a;
+}
+
+void imprimirArbol(arbol *a, arbol *root){
+    
+    
+    printf("españ ol : %s \n", a->esp);
+    printf("ingles : %s \n", a->ing);
+    // padre español
+    if(a->padreEsp != NULL){
+        printf("el nodo padre español : %s/%s \n", root->padreEsp->esp, root->padreEsp->ing);
+    }else {
+        printf("sin padre español \n");
+    }
+    // padre ingles
+    if(a->padreIng != NULL){
+        printf("el nodo padre ingles es: %s/%s \n", root->padreIng->esp, root->padreIng->ing);
+    }else{
+        printf("sin padre ingles \n");
+    }
+    if(a->derechoEsp != NULL){
+        printf("\t hijo derecho : %s/%s \n", a->derechoEsp->esp, a->derechoEsp->ing);
+    }else{
+        printf("\t sin hijo derecho español \n");
+    }
+    if(a->izquierdoEsp != NULL){
+        printf("\t hijo izquierdo : %s/%s \n", a->izquierdoEsp->esp, a->izquierdoEsp->ing);
+    }else{
+        printf("\t sin hijo izquierdo español \n");
+    }
+
+    if(a->derechoIng != NULL){
+        printf("\t hijo derecho : %s/%s \n", a->derechoIng->esp, a->derechoIng->ing);
+    }else{
+        printf("\t sin hijo derecho ingles\n");
+    }
+    if(a->izquierdoIng != NULL){
+        printf("\t hijo izquierdo : %s/%s \n", a->izquierdoIng->esp, a->izquierdoIng->ing);
+    }else{
+        printf("\t sin hijo izquierdo ingles \n");
+    }
+}
+void inorder(arbol *a, arbol* root){
+    if(a == NULL){
+        return;
+    }
+    inorder(a->izquierdoEsp, a, word);
+    a->padreEsp = root;
+    inorder(a->derechoEsp, a, word);
+}
+void inorderIngles(arbol *a, arbol *root){
+    if(a == NULL){
+        return;
+    }
+    inorder(a->izquierdoIng, a, word);
+    a->padreIng = root;
+    inorder(a->derechoIng, a, word);
 }
--- main.c
+++ main.c
@@ -1,78 +1,27 @@
 #include "stdio.h"
 #include "arbol.c"
 
-void procesar(arbol *a, arbol *root, char *palabra, int flag){
-    if(strcmp(a->esp, palabra) == 0){
-        printf("tu palabra traducida es : %s \n", a->ing);
-        printf("el nodo padre es: %s/%s", root->esp, root->ing);
-        if(a->izquierdo != NULL){
-            printf("el nodo hijo izquierdo en idioma español es : %s/%s \n", a->izquierdo->esp, a->izquierdo->esp);
-        }
-        //derecho
-        if(a->derecho != NULL){
-            printf("el nodo hijo derecho en idioma español es: %s/%s \n", a->derecho->esp, a->derecho->ing);
-        }
-    }
-    return;
-}
-void imprimirArbol(arbol *a, arbol *root){
-    printf("root esp : %s \n", root->esp);
-    printf("root ing : %s \n", root->ing);
-    printf("espanol : %s \n", a->esp);
-    printf("ingles : %s \n", a->ing);
-    if(a->derecho != NULL){
-        printf("\t hijo derecho : %s/%s \n", a->derecho->esp, a->derecho->ing);
-    }else{
-        printf("\t sin hijo derecho \n");
-    }
-    if(a->izquierdo != NULL){
-        printf("\t hijo izquierdo : %s/%s \n", a->izquierdo->esp, a->izquierdo->ing);
-    }else{
-        printf("\t sin hijo izquierdo \n");
-    }
-}
-
-void inorder(arbol *a, arbol* root, char *word){
+void inorderAccess(arbol *a){
     if(a == NULL){
         return;
     }
-    inorder(a->izquierdo, a, word);
-    printf("--------- \n");
-    imprimirArbol(a, root);
-    printf("--------- \n");
-    //procesar(a, a, word, 1);
-    inorder(a->derecho, a, word);
+    inorderAccess(a->izquierdoEsp);
+    //proccess
+    inorderAccess(a->derechoIng);
 }
-
-
 
 int main(){
     printf("laboratorio3 !!!  \n");
-    char *option = malloc(1024 * sizeof(char));
-
-    printf("%s   \n \n", option);
+    arbol *a = NULL;
+    a = rellenarArbol();
+/*     char *option = malloc(1024 * sizeof(char));
     arbol *a = NULL;
     arbol *b = NULL;
-    a = rellenarArbol(1);
+    ;
     b = rellenarArbol(0);
-    /* a = agregarHoja(a, "Casa", "House");
-    a = agregarHoja(a, "Zorro", "Fox");
-    a = agregarHoja(a, "Manzana", "Apple");
-    a = agregarHoja(a, "Dedo", "Finger");
-    a = agregarHoja(a, "Jugar", "Play");
-    a = agregarHoja(a, "Oso", "Bear");
-    a = agregarHoja(a, "Arbol", "Tree");
-
-    b = agregarHojaIngles(b, "Casa", "House");
-    b = agregarHojaIngles(b, "Zorro", "Fox");
-    b = agregarHojaIngles(b, "Manzana", "Apple");
-    b = agregarHojaIngles(b, "Dedo", "Finger");
-    b = agregarHojaIngles(b, "Jugar", "Play");
-    b = agregarHojaIngles(b, "Oso", "Bear");
-    b = agregarHojaIngles(b, "Arbol", "Tree"); */
-    inorder(a, a, "Oso");
+    inorder(a, a, "Oso"); */
     // inorder(a, a, )
-    printf("-----------------\n");
+
     return 0;
 
 
